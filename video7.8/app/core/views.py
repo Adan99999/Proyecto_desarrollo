87b21da67a106efa6d3c22c71eeb0884f52baf9e
Ad-Os12 <Abbelr9566@gmail.com>
2021-08-31 00:59:20 -0600

    urls video 14
    comfiguraciones de urls iniciando video 14,,, y abriendo el proyecto,,,,,

--- video7.8/app/core/views.py
+++ video7.8/app/core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+# return HttpResponse('hola esta es nu primera url')
+def myfirstview(request):
+
+    data = {
+        'name': 'Adan'
+    }
+    return JsonResponse(data)
