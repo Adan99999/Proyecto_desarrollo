fadc5f602c1640270fa8b2f2d13fc26310c1f7c4
Ad-Os12 <Abbelr9566@gmail.com>
2021-09-14 03:36:27 -0600

    video 25
    utilizar el metodo ajax,,,
    
    se modifico la url que esta en core,,,,
    path('category/list/', CategoryListView.as_view(), name='vista1'),
    #    path('category/list2/', category_list, name='vista2'),
    
    se cambio por
    
    path('category/list/', CategoryListView.as_view(), name='category_list'),

--- video7.8/app/core/urls.py
+++ video7.8/app/core/urls.py
@@ -4,7 +4,12 @@ from core.views.category.views import *
 
 app_name = 'erp'
 
+#urlpatterns = [
+#    path('category/list/', CategoryListView.as_view(), name='vista1'),
+#    path('category/list2/', category_list, name='vista2'),
+#]
+
 urlpatterns = [
-    path('category/list/', CategoryListView.as_view(), name='vista1'),
-    #path('category/list2/', category_list, name='vista2'),
+    path('category/list/', CategoryListView.as_view(), name='category_list'),
+    path('category/list2/', category_list, name='category_list2'),
 ]
